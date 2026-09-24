bed90c910b77755fac7581f00802660de7663117
Karim Mouminoun <sprytnyka@gmail.com>
2026-09-24 20:11:15 +0200

    creatfile3

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-user = input("Enter name of the file: ")
-filename = user + ".txt"
+def main() -> None:
+    user = input("Enter name of the file: ")
+    filename = user + ".txt"
+    lista = []
+    while True:
+        user2 = input("Enter new line of content: ")
+        if user2 == "stop":
+            break
+        lista.append(user2 + "\n")
+    with open(filename, "w") as file:
+        file.writelines(lista)
 
-lista = []
-while True:
-    user2 = input("Enter new line of content: ")
-    if user2 == "stop":
-        break
-    lista.append(user2 + "\n")
 
-with open(filename, "w") as file:
-    file.writelines(lista)
+if __name__ == "__main__":
+    main()
